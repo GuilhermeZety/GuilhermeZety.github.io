a28c0d6cc922d017c75692376311082e8ecfe12e
Guilherme Martins <guilherme.m.zety@gmail.com>
2024-02-22 14:50:23 -0300

    coisas

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "03a0e1bc81094687d81d2b6b91f1227f",
-"index.html": "bc781b3baa4eb457511f3ecf008a47c4",
-"/": "bc781b3baa4eb457511f3ecf008a47c4",
-"main.dart.js": "8dfb35a2da16076960b6d8ae6a849963",
+"index.html": "9e14cc2d08970436a21faed035317473",
+"/": "9e14cc2d08970436a21faed035317473",
+"main.dart.js": "628d8b5d8943d65381cb73aa046ed231",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "364b5da9ebee6969359531fea9177ac9",
 "icons/Icon-192.png": "3691284d57697921f8ee477643ba68f4",
